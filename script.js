e483cb5f2f34ca802ab70c2f1941095acc06248b
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2024-11-06 09:51:40 +0100

    datos del examen

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const libros = [
+    {
+        titulo: "1984", 
+        autor: "George Orwell", 
+        genero: "Ficción", 
+        anioPublicacion: 1949, 
+        disponible: true 
+    },
+    { 
+        titulo: "Un Mundo Feliz", 
+        autor: "Aldous Huxley",
+        genero: "Ficción", 
+        anioPublicacion: 1932, 
+        disponible: false 
+    },
+    { 
+        titulo: "Sapiens", 
+        autor: "Yuval Noah Harari", 
+        genero: "Historia", 
+        anioPublicacion: 2011,
+        disponible: true 
+    },
+    { 
+        titulo: "El Origen de las Especies", 
+        autor: "Charles Darwin", 
+        genero: "Ciencia", 
+        anioPublicacion: 1859, 
+        disponible: true 
+    },
+    { 
+        titulo: "La Divina Comedia", 
+        autor: "Dante Alighieri", 
+        genero: "Poesía", 
+        anioPublicacion: 1320, 
+        disponible: false 
+    }
+];
